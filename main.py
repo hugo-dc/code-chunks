e092353773b6114746ca559a29e2b4974f77d3da
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2020-08-28 02:37:46 -0500

    Merkleize code

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import math
+from trie import HexaryTrie
+import rlp
+import hashlib
 
 CHUNK_SIZE = 32
 KEY_LENGTH = 4
-METADATA_KEY = "0xffffffff"
+METADATA_KEY = bytes.fromhex("ffffffff")
 METADATA_VERSION = 0
 EMPTY_CODE_ROOT = "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
 HF_BLOCK_NUMBER = None # TBD
 
-bytecode = "00600000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000"
+bytecode = bytes.fromhex("00600000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000")
 
 def get_chunks(bc):
+    print('bytecode: ', bc)
     chunks = []
 
-    while len(bc) >= (CHUNK_SIZE * 2):
+    while len(bc) >= CHUNK_SIZE:
         startOffset = 0
         firstInstructionOffset = 0
-        codeChunk = bc[:CHUNK_SIZE * 2]
+        codeChunk = bc[:CHUNK_SIZE]
         chunks.append({'startOffset': startOffset,
                        'firstInstructionOffset': firstInstructionOffset,
                        'codeChunk': codeChunk})
-        bc = bc[CHUNK_SIZE * 2:]
+        bc = bc[CHUNK_SIZE:]
 
     return chunks
 
 def get_immediates(opcode):
-    if opcode == "60":
+    if opcode == bytes.fromhex("60"):
         return 1
     else:
         return 0
@@ -38,11 +42,11 @@ def second_pass(chunks): # TODO: Use a better name
 
     print('chunks: ', chunks)
 
-    for i in range(int(len(bytecode)/2)):
+    for i in range(len(bytecode)):
         pc = i
         print('pc: ', pc)
-        opcode = bytecode[pc*2:pc*2+2]
-        print('opcode: 0x'+opcode)
+        opcode = bytecode[pc:pc+1]
+        print('opcode: 0x'+opcode.hex())
         n = get_immediates(opcode)
         print('pc + n % CHUNK_SIZE : ', pc + n % CHUNK_SIZE)
         print('pc % CHUNK_SIZE: ', pc % CHUNK_SIZE)
@@ -62,14 +66,41 @@ def second_pass(chunks): # TODO: Use a better name
                 print('nextChunk: ', nextChunk)
     return(chunks)
 
+def format_key(value):
+    byte_array = bytes([value])
+    while len(byte_array) < KEY_LENGTH:
+        byte_array = bytes.fromhex('00') + byte_array
+    return byte_array
+
+def merkleize(chunks): # TODO: Compute codeTrie root according to EIP-2584
+    trie = HexaryTrie(db={})
+    
+    # Process each chunk
+    for c in chunks:
+        key = format_key(c['startOffset'])
+        value = rlp.encode([c['firstInstructionOffset'], c['codeChunk']])
+
+        # insert into codeTrie
+        trie.set(key, value)
+
+        # Insert metadata leaf
+        s = hashlib.sha3_256()
+        s.update(c['codeChunk'])
+        codeHash = s.digest()
+        codeLength = len(c['codeChunk'])
+    
+        key = METADATA_KEY
+        value = rlp.encode([METADATA_VERSION, codeHash, codeLength])
+        trie.set(key, value)
+
+    return trie
+    
+
     
 # Divide the bytecode evenly in CHUNK_SIZE parts.
 chunks = get_chunks(bytecode)
 chunkCount = len(chunks)
 chunks = second_pass(chunks)
+mpt = merkleize(chunks)
 
-# Process each chunk
-for c in chunks:
-    print(c)
-
-
+print('mpt: 0x'+ mpt.root_hash.hex())
