c727523dd42b8be6472b495f08631cb02f5c6964
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2020-09-03 02:13:00 -0500

    Fix hashing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import hashlib
 
+def sha3_256(data):
+    s = hashlib.sha3_256()
+    s.update(data)
+    return s.digest()
+
+    
 def generate_code_root(program):
     tree = treefy(chunkify(program), len(program))
     return tree[0]
@@ -12,13 +18,8 @@ def chunkify(program):
     while pos < len(program):
         pos += (program[pos] - 0x5f) if (0x60 <= program[pos] <= 0x7f) else 1
         if pos >= this_chunk_start + 32:
-            s = hashlib.sha3_256()
-            s.update(program[this_chunk_start:this_chunk_start + 32] + this_chunk_code_start.to_bytes(32, 'big'))
-            digest = s.digest()
-            s = hashlib.sha3_256()
-            s.update(program[this_chunk_start:this_chunk_start + 32] + this_chunk_code_start.to_bytes(32, 'big'))
-            digest = s.digest()
-            chunks.append(digest)
+            result = sha3_256(program[this_chunk_start:this_chunk_start + 32] + this_chunk_code_start.to_bytes(32, 'big'))
+            chunks.append(result)
             this_chunk_start += 32
             this_chunk_code_start = pos
     return chunks
@@ -30,14 +31,10 @@ def treefy(chunks, length):
     padded_length = next_power_of_2(len(chunks))
     tree = [None] * padded_length + chunks + [b'\x00'*32] * (padded_length - len(chunks))
     for i in range(padded_length-1, 0, -1):
-        s = hashlib.sha3_256()
-        s.update(tree[i*2] + tree[i*2+1])
-        digest=s.digest()
-        tree[i] = digest
+        hash = sha3_256(tree[i*2] + tree[i*2+1])
+        tree[i] = hash
     metadata = length.to_bytes(32, 'big')
-    s = hashlib.sha3_256()
-    s.update(tree[1] + metadata)
-    digest = s.digest()
-    tree[0] = digest
+    hash = sha3_256(tree[1] + metadata)
+    tree[0] = hash
     return tree
 
