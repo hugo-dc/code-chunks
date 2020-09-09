44bbe8fa7321f334133fd24e157045dab8fcb35b
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2020-09-09 18:49:42 -0500

    Only show index as label, show hash only when pointer is over the node

--- treeviz.py
+++ treeviz.py
@@ -11,19 +11,21 @@ def show_tree(tree, title='Tree'):
 
 
     for t in subtree:
-        dot.node(str(ix), t.hex())
+        dot.node(str(ix)+'-'+t.hex(), str(ix))
         if iy+1 < len(subtree):
             left = iy+1
             right = iy + 2
-            dot.edge(str(ix), str(iy+1))
-            dot.edge(str(ix), str(iy+2))
+            dot.edge(str(ix)+'-'+t.hex(), str(iy+1)+'-' + subtree[iy+1].hex())
+            dot.edge(str(ix)+'-'+t.hex(), str(iy+2)+'-' + subtree[iy+2].hex())
             iy +=2
         ix += 1
 
-    dot.node("A", tree[0].hex())
+    dot.node("root-"+tree[0].hex(), "root")
     dot.node("B", "metadata")
-    dot.edges(["AB"])
-    dot.edges(["A0"])
+    #dot.edges(["AB"])
+    dot.edge("root-"+tree[0].hex(), "B")
+    #dot.edges(["A0"])
+    dot.edge("root-"+tree[0].hex(), '0-' + subtree[0].hex())
     dot.render('render/' + title + '.dot', view=False)
     return dot
 
